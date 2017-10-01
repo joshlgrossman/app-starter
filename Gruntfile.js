9c27f9adb28f23aa01847a870fee041a303973fd
joshlgrossman <josh.l.grossman@gmail.com>
2017-10-01 14:31:32 -0500

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = grunt => {
+
+  const package = grunt.file.readJSON('package.json');
+  const tsconfig = grunt.file.readJSON('tsconfig.json');
+
+  grunt.initConfig({
+    pkg: package,
+
+    ts: {
+      options: tsconfig.compilerOptions,
+      build: {
+        src: ['src/**/*.ts'],
+        outDir: 'dist/'
+      }
+    },
+
+    browserify: {
+      build: {
+        files: {
+          'dist/bundle.js': 'dist/main.js'
+        }
+      }
+    },
+
+    clean: {
+      build: ['dist/**/*.js', '!dist/bundle.js']
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'dist/bundle.js': 'dist/bundle.js'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', [
+    'ts',
+    'browserify',
+    'clean',
+    'uglify'
+  ]);
+
+};
